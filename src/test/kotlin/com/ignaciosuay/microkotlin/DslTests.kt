5cd2ff428dfbcf77b47e9cada77c8a5e4a8a11e0
Ignacio Suay <ignacio.suay@gmail.com>
2017-07-13 17:57:04 +0100

    add dsl test

--- src/test/kotlin/com/ignaciosuay/microkotlin/DslTests.kt
+++ src/test/kotlin/com/ignaciosuay/microkotlin/DslTests.kt
@@ -23,6 +23,16 @@ class DslTests {
     @Test
     fun testLambdaWithReceiverAndInputParams() {
         val sum = fun Int.(other: Int): Int = this + other
-        assert (3 == 1.sum(2))
+        assert(3 == 1.sum(2))
     }
+
+//    class TABLE {
+//        fun tr(init: TR.() -> Unit)
+//    }
+//
+//    class TR {
+//        fun td(init: TD.() -> Unit)
+//    }
+//
+//    class TD
 }
